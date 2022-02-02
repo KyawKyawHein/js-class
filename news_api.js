323d3c65aa546d5b95a0a882ad1b0b41bb0cbd21
Kyaw Kyaw Hein <hhein6223@gmail.com>
2022-02-02 20:46:53 +0630

    js class

---
+++ news_api.js
@@ -0,0 +1,266 @@
+let data = {
+    "status": "ok",
+    "totalResults": 13526,
+    "articles": [
+    {
+    "source": {
+    "id": "business-insider",
+    "name": "Business Insider"
+    },
+    "author": "hrobertson@businessinsider.com (Harry Robertson)",
+    "title": "Bitcoin's rise despite the UK crackdown on Binance is a bullish signal that prices may have bottomed, Fundstrat says",
+    "description": "Bitcoin \"rising in the face of bad news may be a sign of the seller exhaustion we need to go higher,\" Fundstrat said.",
+    "url": "https://markets.businessinsider.com/currencies/news/bitcoin-price-outlook-binance-bullish-uk-crackdown-rise-fundstrat-2021-6-1030562958",
+    "urlToImage": "https://images2.markets.businessinsider.com/60d1f4bec097fe00185a230a?format=jpeg",
+    "publishedAt": "2021-06-29T13:03:08Z",
+    "content": "Bitcoin has fallen sharply since May.\r\nREUTERS/Dado Ruvic\r\nBitcoin's rise in the face of the UK's crackdown on major crypto exchange Binance is a bullish signal that suggests prices may have bottomed… [+1964 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "The Truth About Cars"
+    },
+    "author": "Tim Healey",
+    "title": "QOTD: Would You Drive a Yoke?",
+    "description": "Tesla’s steering yoke is making news again, sort of, in part because of a video circulating Twitter and in part because our competition over at Jalopnik took some bad-faith criticism over a minor mistake. I won’t get too deep into the weeds over the Jalopnik …",
+    "url": "https://www.thetruthaboutcars.com/2021/06/qotd-would-you-drive-a-yoke/",
+    "urlToImage": "https://www.thetruthaboutcars.com/wp-content/uploads/2021/06/0x0-ModelS_07-610x407.jpg",
+    "publishedAt": "2021-06-29T13:00:31Z",
+    "content": "Tesla’s steering yoke is making news again, sort of, in part because ofÂ a video circulating Twitter and in part because our competition over atÂ JalopnikÂ took some bad-faith criticism over a minor … [+1522 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Slashdot.org"
+    },
+    "author": "feedfeeder",
+    "title": "Stocks making the biggest moves in the premarket: Big banks, Facebook, Tesla & more - CNBC",
+    "description": "Stocks making the biggest moves in the premarket: Big banks, Facebook, Tesla & moreCNBC Wall Street banks hike shareholder payouts after Fed gives the green lightReuters Banks Ready to Payback Shareholders After Stress TestsBloomberg Markets and Finance Morga…",
+    "url": "https://slashdot.org/firehose.pl?op=view&amp;id=148514564",
+    "urlToImage": null,
+    "publishedAt": "2021-06-29T12:52:55Z",
+    "content": "The Fine Print: The following comments are owned by whoever posted them. We are not responsible for them in any way."
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Sopitas.com"
+    },
+    "author": "Greta Padilla",
+    "title": "Bienvenido al gremio: Facebook ahora vale 1 billón de dólares y entra al ranking en 5to lugar",
+    "description": "Han de saber que antes de iniciar esta nota, recordamos que billion (en inglés) no se traduce como “billón”, sino como mil millones. Por lo tanto, cuando leímos que Facebook “is now a $1 trillion dollar company“, investigamos su valor en términos en español… …",
+    "url": "https://www.sopitas.com/mientras-tanto/facebook-vale-mercado-1-billon-dolares-compania-tecnologia/",
+    "urlToImage": "https://i1.wp.com/www.sopitas.com/wp-content/uploads/2021/06/facebook-vale-billones-tecnología-scaled.jpg",
+    "publishedAt": "2021-06-29T12:50:48Z",
+    "content": "Facebook entró al ranking de las cinco compañías de tecnología que superan en su valor el billón de dólares (trillion en inglés).\r\nHan de saber que antes de iniciar esta nota, recordamos que billion … [+2754 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "PCMag.com"
+    },
+    "author": "Pc Mag Staff",
+    "title": "Ρεκόρ 69.420 ταυτόχρονα ενεργών χρηστών σημείωσε το Starlink",
+    "description": "Η υπηρεσία παροχής δορυφορικού internet Starlink που ανήκει στον δισεκατομμυριούχο Elon Musk σημειώνει αρκετά ανοδική πορεία τους τελευταίους μήνες.",
+    "url": "https://gr.pcmag.com/networking/36763/rekor-69420-tautokhrona-energon-khreston-semeiose-to-starlink",
+    "urlToImage": "https://sm.pcmag.com/t/pcmag_gr/news/s/spacexs-st/spacexs-starlink-tops-69420-concurrent-users_cjay.1200.jpg",
+    "publishedAt": "2021-06-29T12:45:57Z",
+    "content": "(Credit: SpaceX)\r\n internet Starlink 69,000 tweet CEO Elon Musk, .\r\n, : « Starlink 69,420 », ( 69 , 420 ). CEO, 2020 Tesla Model S, $69,420.\r\nStarlink 10,000 , , , 50 150Mbps.\r\ntweet , Musk 72 , , .\r… [+137 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Tinhte.vn"
+    },
+    "author": "rubilee274@gmail.com (Rubi Lee), Rubi Lee",
+    "title": "Vị tỷ phú giàu thứ 3 thế giới - Elon Musk đang sống tại một căn nhà lắp ghép chỉ có giá 1,15 tỷ đồng",
+    "description": "Trong khi các vị đại gia tỷ phú thường tận hưởng cuộc sống xa hoa tại những khu đất vàng, nơi bất động sản ở mức giá khổng lồ. Thay vào đó, Elon Musk giám đốc điều hành của công ty Tesla, còn được...",
+    "url": "https://tinhte.vn/thread/vi-ty-phu-giau-thu-3-the-gioi-elon-musk-dang-song-tai-mot-can-nha-lap-ghep-chi-co-gia-1-15-ty-dong.3356222/",
+    "urlToImage": "https://photo2.tinhte.vn/data/attachment-files/2021/06/5528686_cover.jpg",
+    "publishedAt": "2021-06-29T12:38:45Z",
+    "content": "Các thông tin cho thy Elon Musk sng trong mt ngôi nhà Boxabl ã xut hin t tháng 11/2020, khi công ty xây dng Boxabl ng ti mt video bên trong cn h mà h va xây dng Boca Chica, Texas. Ni dung on video ch… [+874 chars]"
+    },
+    {
+    "source": {
+    "id": "independent",
+    "name": "Independent"
+    },
+    "author": "Anthony Cuthbertson",
+    "title": "Dogecoin sent ‘to space’ for Elon Musk’s birthday",
+    "description": "SpaceX boss plans to send dogecoin ‘to the actual moon’ in 2022",
+    "url": "https://www.independent.co.uk/life-style/gadgets-and-tech/dogecoin-space-elon-musk-spacex-b1874571.html",
+    "urlToImage": "https://static.independent.co.uk/2021/06/29/09/dogecoin%20space%20elon%20musk.jpg?width=1200&auto=webp&quality=75",
+    "publishedAt": "2021-06-29T12:37:35Z",
+    "content": "A YouTuber has sent a dogecoin high up towards the Earths stratosphere in a stunt designed to take the cryptocurrency to space.\r\nReid Williamson used a weather balloon to launch the replica token in … [+1844 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Yahoo Entertainment"
+    },
+    "author": "Asma UL Husna",
+    "title": "Hedge Funds Are Coming Back To International Game Technology PLC (IGT)",
+    "description": "With the first-quarter round of 13F filings behind us it is time to take a look at the stocks in which some of the best money managers in the world preferred...",
+    "url": "https://finance.yahoo.com/news/hedge-funds-coming-back-international-123424223.html",
+    "urlToImage": "https://s.yimg.com/uu/api/res/1.2/hRc3uvgKA14nDpMuBXkYjQ--~B/aD00NDQ7dz0zNTU7YXBwaWQ9eXRhY2h5b24-/https://media.zenfs.com/en/insidermonkey.com/55b6b02c2e68609149f1dade6b8b0f2c",
+    "publishedAt": "2021-06-29T12:34:24Z",
+    "content": "With the first-quarter round of 13F filings behind us it is time to take a look at the stocks in which some of the best money managers in the world preferred to invest or sell heading into the second… [+6295 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Yahoo Entertainment"
+    },
+    "author": "Abigail Fisher",
+    "title": "Where Do Hedge Funds Stand On The Trade Desk, Inc. (TTD)?",
+    "description": "How do you pick the next stock to invest in? One way would be to spend days of research browsing through thousands of publicly traded companies. However, an ...",
+    "url": "https://finance.yahoo.com/news/where-hedge-funds-stand-trade-123421195.html",
+    "urlToImage": "https://s.yimg.com/uu/api/res/1.2/VFIvepRv_dTLck0n.Amj_g--~B/aD02MDA7dz00NzM7YXBwaWQ9eXRhY2h5b24-/https://media.zenfs.com/en/insidermonkey.com/33ede3b2a4dafa7bc9d5d35b0755b9e1",
+    "publishedAt": "2021-06-29T12:34:21Z",
+    "content": "How do you pick the next stock to invest in? One way would be to spend days of research browsing through thousands of publicly traded companies. However, an easier way is to look at the stocks that s… [+6406 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Yahoo Entertainment"
+    },
+    "author": "Abigail Fisher",
+    "title": "Here is What Hedge Funds Think About Ingersoll Rand Inc. (IR)",
+    "description": "A whopping number of 13F filings filed with U.S. Securities and Exchange Commission has been processed by Insider Monkey so that individual investors can...",
+    "url": "https://finance.yahoo.com/news/hedge-funds-think-ingersoll-rand-123417963.html",
+    "urlToImage": "https://s.yimg.com/uu/api/res/1.2/Er_LQdNXhRPP9UNuRe1Ntg--~B/aD02MDA7dz00ODM7YXBwaWQ9eXRhY2h5b24-/https://media.zenfs.com/en/insidermonkey.com/65226e503317b1fa49f3e6e9f0748c8e",
+    "publishedAt": "2021-06-29T12:34:17Z",
+    "content": "A whopping number of 13F filings filed with U.S. Securities and Exchange Commission has been processed by Insider Monkey so that individual investors can look at the overall hedge fund sentiment towa… [+6015 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Yahoo Entertainment"
+    },
+    "author": "Asma UL Husna",
+    "title": "Hedge Funds Have Never Been This Bullish On Canadian National Railway Company (CNI)",
+    "description": "In this article we will analyze whether Canadian National Railway Company (NYSE:CNI) is a good investment right now by following the lead of some of the best...",
+    "url": "https://finance.yahoo.com/news/hedge-funds-never-bullish-canadian-123413477.html",
+    "urlToImage": "https://s.yimg.com/uu/api/res/1.2/nClwIzbwRS9bs0Y8oOC2rw--~B/aD02MDA7dz00NTA7YXBwaWQ9eXRhY2h5b24-/https://media.zenfs.com/en/insidermonkey.com/8ff592813346f4e1d547a73e73cee9d4",
+    "publishedAt": "2021-06-29T12:34:13Z",
+    "content": "In this article we will analyze whether Canadian National Railway Company (NYSE:CNI) is a good investment right now by following the lead of some of the best investors in the world and piggybacking t… [+7202 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Yahoo Entertainment"
+    },
+    "author": "Abigail Fisher",
+    "title": "Hedge Funds Are Cashing Out Of Alteryx, Inc. (AYX)",
+    "description": "Before we spend countless hours researching a company, we like to analyze what insiders, hedge funds and billionaire investors think of the stock first. This...",
+    "url": "https://finance.yahoo.com/news/hedge-funds-cashing-alteryx-inc-123410124.html",
+    "urlToImage": "https://s.yimg.com/uu/api/res/1.2/7a6P7Xx1RKb_4npCCjFc4A--~B/aD02MDA7dz00NzM7YXBwaWQ9eXRhY2h5b24-/https://media.zenfs.com/en/insidermonkey.com/eaefab3c2348f7eb7a6e39d82f530fd4",
+    "publishedAt": "2021-06-29T12:34:10Z",
+    "content": "Before we spend countless hours researching a company, we like to analyze what insiders, hedge funds and billionaire investors think of the stock first. This is a necessary first step in our investme… [+6487 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Yahoo Entertainment"
+    },
+    "author": "Asma UL Husna",
+    "title": "Hedge Funds Are Betting On Royal Dutch Shell plc (RDS)",
+    "description": "Is Royal Dutch Shell plc (NYSE:RDS) a good place to invest some of your money right now? We can gain invaluable insight to help us answer that question by...",
+    "url": "https://finance.yahoo.com/news/hedge-funds-betting-royal-dutch-123407895.html",
+    "urlToImage": "https://s.yimg.com/uu/api/res/1.2/HGg5EzSzX5kwRPqNXzVCEA--~B/aD02MDA7dz00NzM7YXBwaWQ9eXRhY2h5b24-/https://media.zenfs.com/en/insidermonkey.com/ee079a8c69165359cb3e1ebf8acf5a6c",
+    "publishedAt": "2021-06-29T12:34:07Z",
+    "content": "Is Royal Dutch Shell plc (NYSE:RDS) a good place to invest some of your money right now? We can gain invaluable insight to help us answer that question by studying the investment trends of top invest… [+6191 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Yahoo Entertainment"
+    },
+    "author": "Abigail Fisher",
+    "title": "Should I Buy Huntsman Corporation (HUN)?",
+    "description": "The latest 13F reporting period has come and gone, and Insider Monkey is again at the forefront when it comes to making use of this gold mine of data. We at ...",
+    "url": "https://finance.yahoo.com/news/buy-huntsman-corporation-hun-123404288.html",
+    "urlToImage": "https://s.yimg.com/uu/api/res/1.2/cnPQxUCR6AFVCnnj2TRufw--~B/aD02MDA7dz00NTA7YXBwaWQ9eXRhY2h5b24-/https://media.zenfs.com/en/insidermonkey.com/e8edef4050b4f7c21a734248fd880ea0",
+    "publishedAt": "2021-06-29T12:34:04Z",
+    "content": "The latest 13F reporting period has come and gone, and Insider Monkey is again at the forefront when it comes to making use of this gold mine of data. We at Insider Monkey have plowed through 866 13F… [+7036 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Yahoo Entertainment"
+    },
+    "author": "Asma UL Husna",
+    "title": "Hedge Funds Are Nibbling On Sensata Technologies Holding plc (ST)",
+    "description": "While the market driven by short-term sentiment influenced by the accommodative interest rate environment in the US, virus news and stimulus spending, many...",
+    "url": "https://finance.yahoo.com/news/hedge-funds-nibbling-sensata-technologies-123400633.html",
+    "urlToImage": "https://s.yimg.com/uu/api/res/1.2/.1fTP4EUmD0lrN7iarPcCQ--~B/aD02MDA7dz00NTA7YXBwaWQ9eXRhY2h5b24-/https://media.zenfs.com/en/insidermonkey.com/977921eb3bd09c458921558f759fad12",
+    "publishedAt": "2021-06-29T12:34:00Z",
+    "content": "While the market driven by short-term sentiment influenced by the accommodative interest rate environment in the US, virus news and stimulus spending, many smart money investors are starting to get c… [+7383 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Yahoo Entertainment"
+    },
+    "author": "Asma UL Husna",
+    "title": "Is LKQ Corporation (LKQ) Going to Burn These Hedge Funds?",
+    "description": "In this article we will take a look at whether hedge funds think LKQ Corporation (NASDAQ:LKQ) is a good investment right now. We check hedge fund and...",
+    "url": "https://finance.yahoo.com/news/lkq-corporation-lkq-going-burn-123357287.html",
+    "urlToImage": "https://s.yimg.com/uu/api/res/1.2/DeHEGj29fgn9IkuLw0WrjA--~B/aD02MDA7dz00OTg7YXBwaWQ9eXRhY2h5b24-/https://media.zenfs.com/en/insidermonkey.com/2d458471a3784ef5d4286c9bfbfee1ea",
+    "publishedAt": "2021-06-29T12:33:57Z",
+    "content": "In this article we will take a look at whether hedge funds think LKQ Corporation (NASDAQ:LKQ) is a good investment right now. We check hedge fund and billionaire investor sentiment before delving int… [+5722 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Daily Mail"
+    },
+    "author": "Emily Crane",
+    "title": "AOC faces backlash for saying concerns over crime spike is 'hysteria'",
+    "description": "The New York congresswoman made the comment on Sunday while discussing  concerns  of the rising crime wave across the country.",
+    "url": "https://www.dailymail.co.uk/news/article-9736879/AOC-faces-backlash-saying-concerns-crime-spike-hysteria.html",
+    "urlToImage": "https://i.dailymail.co.uk/1s/2021/06/29/13/44815451-0-image-a-17_1624969432587.jpg",
+    "publishedAt": "2021-06-29T12:31:06Z",
+    "content": "Alexandria Ocasio-Ortez has come under fire for saying there was 'hysteria' across the United States over the surging violent crime rates, including a 64 percent spike in shootings in her native New … [+5779 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Motley Fool"
+    },
+    "author": "newsfeedback@fool.com (Will Ebiefung)",
+    "title": "2 Top Value Stocks to Buy Right Now",
+    "description": "General Motors and Philip Morris International offer outstanding value in their respective industries.",
+    "url": "https://www.fool.com/investing/2021/06/29/2-top-value-stocks-to-buy-right-now/",
+    "urlToImage": "https://g.foolcdn.com/editorial/images/631965/value-stock-auto.jpg",
+    "publishedAt": "2021-06-29T12:31:00Z",
+    "content": "With the S&amp;P 500 trading at an average price-to-earnings (P/E) multiple of 37, stocks right now are generally trading at a significantly more expensive ratio than their historical average of 16. … [+4192 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "mobiFlip.de"
+    },
+    "author": "Oliver Schwuchow",
+    "title": "Porsche Macan: Elektrischer SUV orientiert sich Innen am Taycan",
+    "description": "Das nächste Elektroauto von Porsche wird ein SUV, und zwar kommt ein rein elektrischer Macan. Der Bestseller war mal für 2022 als rein elektrische Version […]",
+    "url": "https://www.mobiflip.de/shortnews/porsche-macan-elektrisch-suv-innen-taycan/",
+    "urlToImage": "https://i0.wp.com/www.mobiflip.de/wp-content/uploads/2021/06/porsche-taycan-interieur.jpg?fit=1200%2C825&ssl=1",
+    "publishedAt": "2021-06-29T12:30:38Z",
+    "content": "Das nächste Elektroauto von Porsche wird ein SUV, und zwar kommt ein rein elektrischer Macan. Der Bestseller war mal für 2022 als rein elektrische Version geplant, doch Porsche bestätigte im Mai, das… [+1416 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "CNBC"
+    },
+    "author": "Matthew J. Belvedere",
+    "title": "What to watch today: Stocks set to open flat after new S&P 500, Nasdaq record closes",
+    "description": "U.S. stock futures were steady Tuesday after Facebook's first close above a $1 trillion market value broadly boosted tech stocks.",
+    "url": "https://www.cnbc.com/2021/06/29/what-to-watch-stocks-set-to-open-flat-after-sp-500-nasdaq-records.html",
+    "urlToImage": "https://image.cnbcfm.com/api/v1/image/106898975-1623950092202Skillsoft-OB-Photo-210617-PRESS-4-jpg?v=1623950138",
+    "publishedAt": "2021-06-29T12:29:56Z",
+    "content": "United Airlines (UAL) unveiled its largest aircraft order ever Tuesday as the carrier charts a path for post-pandemic growth. United plans to buy 200 Boeing Max jets. United ordered 70 a321neos from … [+4649 chars]"
+    }
+    ]
+    }
